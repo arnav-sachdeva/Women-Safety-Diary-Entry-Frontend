9725b3b78f38d463dcac063a4a0f72911dfd32d0
arnav-sachdeva <66235421+arnav-sachdeva@users.noreply.github.com>
2022-12-08 23:56:00 +0530

    Add files via upload

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/Dashboard.css';
 import Notes from  './Notes'
 import Textbox from "./Textbox";
 import img1 from "./css/bg.png";
 
+
+   
+
+  
+ 
+
+    
 export default function Dashboard(props){
+
+    var lat;
+    var lon;
+    
+    navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        console.log("Latitude is :", lat);
+        console.log("Longitude is :", lon);
+      });
+
+
+    function handleSos(lat,lon){
+       
+       
+       
+        fetch("http://localhost:5000/notes/sos",
+        {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin":true
+            },
+            mode: "cors",
+            body: JSON.stringify({
+                lat:lat,
+                lon:lon
+            })
+        })
+        .then(()=>{
+            // after adding the note, reload the page to reflect the changes
+            // alert("SOS SENT");
+        })
+        .catch(err => {
+            throw(err)
+        });
+        alert("SOS SENT");
+    }
+
 
     // When loading, show loading svg
     if (props.isLoading) return (
@@ -31,12 +78,14 @@ export default function Dashboard(props){
                 <img className="Google-Icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='google'/>
                 <div className="Btn-Text">Sign in with Google</div>
             </div>
-            <div>
-               <a href=""><p>Terms & Conditions</p></a> 
-                <a href=""><p>Privacy Policy</p></a> 
-                
+            <div className="sos">
+                <button><a onClick={()=>{
+                    handleSos(lat,lon)
+                }}>SOS</a></button>
             </div>
+           
         </div>
+        
     )
     // If signed in, show the regular dashboard with notes and textbox
     else if(props.isSignedIn) return(   
